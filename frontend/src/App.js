ede46d95baf49fe3a119996e78ae042bc2622148
Hariharan A <120185640+Hariharan-A-1268@users.noreply.github.com>
2025-03-26 15:13:25 +0530

    Modified File Names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/homePage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
         </Routes>
       </Router>
     </div>
