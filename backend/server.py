22b02fd39edf19aec598a92522f68ee5f10c2922
Hariharan A <hariharananbalagan1268@gmail.com>
2025-03-30 14:11:24 +0530

    Fixed Backend Connection

--- backend/server.py
+++ backend/server.py
@@ -7,6 +7,8 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from flask_cors import CORS
 from dotenv import load_dotenv
 
+#pip install flask flask-bcrypt flask-jwt-extended flask-cors python-dotenv sqlitecloud sqlalchemy-sqlitecloud
+
 # Load environment variables
 load_dotenv()
 
