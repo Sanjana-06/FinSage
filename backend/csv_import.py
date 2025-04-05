16577fc109fd20dd8195cfa9a1488f725fcbec65
Hariharan A <120185640+Hariharan-A-1268@users.noreply.github.com>
2025-04-05 21:40:53 +0530

    Update csv_import.py

--- backend/csv_import.py
+++ backend/csv_import.py
@@ -6,30 +6,31 @@ conn = sqlite3.connect("innovate.db")  # Replace with your database name
 cursor = conn.cursor()
 
 # Step 2: Create a Table in SQLite
-table_name = "rd"  # Replace with your table name
+table_name = "future_gold"  # Replace with your table name
 # create_table_query = f"""
 # CREATE TABLE IF NOT EXISTS {table_name} (
-#     Bank VARCHAR(200),
-#     Year_1 NUMERIC,
-#     Year_3 NUMERIC,
-#     Year_5 NUMERIC
+#     Date TEXT,
+#     Price NUMERIC
 # );
-# """  # Replace column definitions with your actual table structure
+# # """  # Replace column definitions with your actual table structure
 # cursor.execute(create_table_query)
 
 # Step 3: Open the CSV File
-# csv_file_path = "rd_interest_rates.csv"  # Replace with the path to your CSV file
-# with open(csv_file_path, "r") as csvfile:
-#     csv_reader = csv.reader(csvfile)
-#     headers = next(csv_reader)  # Assuming the first row is headers
+csv_file_path = "Future Gold Predictions.csv"  # Replace with the path to your CSV file
+with open(csv_file_path, "r") as csvfile:
+    csv_reader = csv.reader(csvfile)
+    headers = next(csv_reader)  # Assuming the first row is headers
     
-#     # Step 4: Insert Data into the Table
-#     for row in csv_reader:
-#         insert_query = f"INSERT INTO {table_name}(Bank, Year_1, Year_3, Year_5) VALUES (?, ?, ?, ?);"  # Adjust placeholders based on your columns
-#         cursor.execute(insert_query, row)
+    # Step 4: Insert Data into the Table
+    for row in csv_reader:
+        insert_query = f"INSERT INTO {table_name} VALUES (?, ?);"  # Adjust placeholders based on your columns
+        cursor.execute(insert_query, row)
+
+# Delete rows where date has slashes
+# cursor.execute("DELETE FROM future_gold WHERE date LIKE '%';")
 
 # Step 5: Commit and Close
 conn.commit()
 conn.close()
 
-print("CSV data imported successfully!")
+print("Operation success!")
